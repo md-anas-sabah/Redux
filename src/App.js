144654258cb9a7eea93b90dffa2cfb9f10914536
Md Anas Sabah <mdanassabah@gmail.com>
2022-03-06 22:20:23 +0530

    Dispatched Action

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./App.css";
-import { useState } from "react";
+// import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>The Count is : {count} </h1>
-      <button onClick={() => setCount(count + 1)}>Increment </button>
-      <button onClick={() => setCount(count - 1)}>Decrement </button>
+      <button onClick={() => dispatch(increment())}>Increment </button>
+      <button onClick={() => dispatch(decrement())}>Decrement </button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>
+        Increment By 10{" "}
+      </button>
     </div>
   );
 }
